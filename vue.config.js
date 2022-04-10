87fb8f8350f406b8ceecf64e41d22a743d57518f
eunchap <ksme7918@gmail.com>
2022-04-10 21:00:49 +0900

    4

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 /*module.exports = defineConfig({
   transpileDependencies: true
-})*/
+})
 module.exports = {
   publicPath: '/vuepage',
   outputDir: './docs'
+}*/
+
+module.exports = { 
+  publicPath: process.env.NODE_ENV = 'production' 
+  ?  '/vuepage/' 
+  : '/', 
+  outputDir: './docs' 
 }
